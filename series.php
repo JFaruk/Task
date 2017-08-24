4ca83bc776a97333a1bb9c8361bfb9fcb603426e
USER\userpc <abdullahfar6@gmail.com>
2017-08-24 14:07:51 +0600

    Even and odd number of 1 to 100 in PHP

---
+++ series.php
@@ -0,0 +1,31 @@
+<?php
+	
+	$n=100;
+	for ($i=1; $i<=$n;$i++)
+	{
+		if($i%2 == 0)
+		{
+		 echo "Even Numbers = " .$i. "</br>";
+		}
+		else 
+		{
+			if($i%2 !== 0)
+			{
+				echo "Odd numbers = " .$i. "</br>";
+			}
+			
+		}
+	
+	
+	}
+	
+
+
+
+?>
+	
+	
+	
+	
+
+

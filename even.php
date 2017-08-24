ae0612e033178ecd1b697f2674db43da4fbfc73f
USER\userpc <abdullahfar6@gmail.com>
2017-08-24 13:36:46 +0600

    Summation of  Even

---
+++ even.php
@@ -0,0 +1,13 @@
+<?php
+	$sum =0;
+	 for($i=1;$i<50;$i++)
+	 {
+		 if($i%2 ==0){
+			 echo "Even number= ".$i."</br>";
+		 $sum +=$i;}
+	 }
+	 
+	 echo "Summation of all even number between 1 to 50 is =". $sum. "</br>";
+
+
+?>

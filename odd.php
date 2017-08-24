af2cab0c56a1f7ac78c1092fb6a60fdfe44c1327
USER\userpc <abdullahfar6@gmail.com>
2017-08-24 12:54:57 +0600

    Little change

--- odd.php
+++ odd.php
@@ -3,11 +3,11 @@
 	 for($i=1;$i<50;$i++)
 	 {
 		 if($i%2 !==0){
-			 echo "Even number= ".$i."</br>";
+			 echo "ODD number= ".$i."</br>";
 		 $sum +=$i;}
 	 }
 	 
-	 echo "". $sum. "</br>";
+	 echo "Summation of all Odd number between 1 to 50 is". $sum. "</br>";
 
 
 ?>

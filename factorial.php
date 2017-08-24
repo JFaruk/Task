493707cf628c99de1f5a3165b46b0d864ee96a27
Junaeb Abdullah faruk <31274734+JFaruk@users.noreply.github.com>
2017-08-24 23:46:29 +0600

    Seperate

--- factorial.php
+++ factorial.php
@@ -1,23 +1,12 @@
 <?php 
 	//factorial of 9
 
-	/*function factorial_n($number) {          
+	function factorial_n($number) {          
         if ($number <=1) { 
             return 1; 
         } else { 
             return ($number * factorial_n($number-1)); 
         } 
     }
-	echo "Factorial of 9 is =" .factorial_n(9); */
-	
-	
-	$n=10;
-	$result=1;
-	$i=1;
-	for($i=1;$i<=$n;$i++){
-		$result=$result*$i;
-		echo "Factorial of 1-10 :" .$result. "</br>";
-	
-	}
-	
-?>
+	echo "Factorial of 9 is =" .factorial_n(9); 
+	?>
